e57978493051fd05dbeffe275649d1c132a81fe6
vladimsov <lonker.hass@yandex.ru>
2026-07-07 16:36:01 +0300

    добавил бэкенд при запуске контейнеров

--- 02-todo/README.md
+++ 02-todo/README.md
@@ -1,5 +1,3 @@
 # Запуск
 
-docker build -t todo .
-
-docker run -p 80:80 todo
+docker-compose up -d
--- 02-todo/frontend/vite.config.js
+++ 02-todo/frontend/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
+import { fileURLToPath } from "url";
+import path from "path";
 
 export default defineConfig({
   plugins: [
@@ -25,7 +27,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": new URL("./src", import.meta.url).pathname,
+      "@": path.resolve(fileURLToPath(new URL(".", import.meta.url)), "src"),
     },
   },
   server: {
